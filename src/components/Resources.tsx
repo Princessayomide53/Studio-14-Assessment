a9a1c368b5d88f06a1337c7ef4366a7c0cac7531
Princess Ayomide Ogunnaike <Payomide53@gmail.com>
2025-08-15 23:10:19 +0100

    Fixed an alignment

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -74,7 +74,7 @@ const Resources = () => {
                 position='absolute'
                 top={-3}
                 bottom={0}
-                left={index === 5 ? '24' : index === 4 ? '-45px' : '' }
+                left={index === 5 ? '24px' : index === 4 ? '-45px' : '' }
                 right={index === 1 ? '9' : index === 3 ? '16px'  : index === 0 || index === 2 ? '0px' : ''}
                 height={index === 5 ? '45%' : '40%'}
               >
